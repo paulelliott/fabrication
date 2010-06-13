2351f32bc48d3a32a5cd62e02e77c068fa57c916
Paul Elliott <paul@hashrocket.com>
2010-06-12 22:27:13 -0400

    Add contexts to specs

--- spec/simple_object_spec.rb
+++ spec/simple_object_spec.rb
@@ -2,32 +2,36 @@ require 'spec_helper'
 
 describe Fabrication do
 
-  let(:person) do
-    Fabricate(:person, :last_name => 'Awesome')
-  end
+  context 'static fields' do
 
-  before(:all) do
-    Fabricator(:person) do
-      first_name 'Joe'
-      last_name 'Schmoe'
-      age 78
+    let(:person) do
+      Fabricate(:person, :last_name => 'Awesome')
     end
-  end
 
-  it 'has the default first name' do
-    person.first_name.should == 'Joe'
-  end
+    before(:all) do
+      Fabricator(:person) do
+        first_name 'Joe'
+        last_name 'Schmoe'
+        age 78
+      end
+    end
 
-  it 'has an overridden last name' do
-    person.last_name.should == 'Awesome'
-  end
+    it 'has the default first name' do
+      person.first_name.should == 'Joe'
+    end
 
-  it 'has the default age' do
-    person.age.should == 78
-  end
+    it 'has an overridden last name' do
+      person.last_name.should == 'Awesome'
+    end
 
-  it 'generates a fresh object every time' do
-    Fabricate(:person).should_not == person
+    it 'has the default age' do
+      person.age.should == 78
+    end
+
+    it 'generates a fresh object every time' do
+      Fabricate(:person).should_not == person
+    end
+
   end
 
 end
