295ee1b5ca7703a9ae2980f7fce01cd868e3d97e
Paul Elliott <paul@hashrocket.com>
2010-06-13 22:55:29 -0400

    Lazily generate block assignments

--- lib/fabrication/proxy.rb
+++ lib/fabrication/proxy.rb
@@ -12,15 +12,32 @@ class Fabrication::Proxy
     @instance
   end
 
-  def method_missing(method, *args, &block)
-    args = [block.call] if block
-    assign(@instance, method, args.first)
+  def method_missing(method_name, *args, &block)
+    if block
+      # copy the original getter
+      @instance.instance_variable_set("@__#{method_name}_original", @instance.method(method_name).clone)
+
+      # store the block for lazy generation
+      @instance.instance_variable_set("@__#{method_name}_block", block)
+
+      # redefine the getter
+      @instance.instance_eval %<
+        def #{method_name}
+          if @__#{method_name}_original.call.nil?
+            send(:#{method_name}=, @__#{method_name}_block.call)
+          end
+          @__#{method_name}_original.call
+        end
+      >
+    else
+      assign(@instance, method_name.to_s, args.first)
+    end
   end
 
   private
 
-  def assign(instance, attribute, *value)
-    instance.send("#{attribute.to_s}=", value.first)
+  def assign(instance, method_name, value)
+    instance.send("#{method_name.to_s}=", value)
   end
 
   #Stolen directly from factory_girl. Thanks thoughtbot!
