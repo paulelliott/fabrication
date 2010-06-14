a347ddf992abe1899c0e470049eb2b5be8bdb894
Dave Ott and Paul Elliott <dev+daveott+paulelliott@hashrocket.com>
2010-06-14 15:35:36 -0400

    Rearrange autoloads and Fabrication module

--- lib/fabrication.rb
+++ lib/fabrication.rb
@@ -1,26 +1,29 @@
 module Fabrication
 
-  extend self
+  autoload :Fabricator, 'lib/fabrication/fabricator'
+  autoload :Fabricate,  'lib/fabrication/fabricate'
+  autoload :Proxy,      'lib/fabrication/proxy'
 
-  def schematic(name, &block)
-    fabricators[name] = Fabricator.new(name, &block)
-  end
 
-  def generate(name, options)
-    fabricators[name].fabricate(options)
-  end
+  class << self
 
-  private
+    def schematic(name, &block)
+      fabricators[name] = Fabricator.new(name, &block)
+    end
 
-  def fabricators
-    @@fabricators ||= {}
+    def generate(name, options)
+      fabricators[name].fabricate(options)
+    end
+
+    private
+
+    def fabricators
+      @@fabricators ||= {}
+    end
+
   end
 
 end
-
-Fabrication.autoload :Fabricator, 'lib/fabrication/fabricator.rb'
-Fabrication.autoload :Fabricate,  'lib/fabrication/fabricate.rb'
-Fabrication.autoload :Proxy,      'lib/fabrication/proxy.rb'
 
 def Fabricator(name, &block)
   Fabrication.schematic(name, &block)
