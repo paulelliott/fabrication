fe73456d3dadc6ba3b18db6675ba9ad3a78ed64e
Paul Elliott <paul@hashrocket.com>
2010-06-19 20:36:35 -0400

    Auto load fabricator definitions

--- lib/fabrication.rb
+++ lib/fabrication.rb
@@ -15,7 +15,26 @@ module Fabrication
     end
 
     def generate(name, options)
+      find_definitions if fabricators.empty?
       fabricators[name].fabricate(options)
+    end
+
+    def find_definitions
+      fabricator_file_paths = [
+        File.join('test', 'fabricators'),
+        File.join('spec', 'fabricators')
+      ]
+      fabricator_file_paths.each do |path|
+        if File.exists?("#{path}.rb")
+          require("#{path}.rb") 
+        end
+
+        if File.directory? path
+          Dir[File.join(path, '*.rb')].each do |file|
+            require file
+          end
+        end
+      end
     end
 
     private
