9e4f40e6fe5a070659cd5a08959a2a3ef19d95f3
Paul Elliott <paul@hashrocket.com>
2010-08-13 10:54:05 -0400

    Add Support.class_for spec

---
+++ spec/support_spec.rb
@@ -0,0 +1,37 @@
+require 'spec_helper'
+
+describe Fabrication::Support do
+
+  describe ".class_for" do
+
+    context "with a class that exists" do
+
+      it "returns the class for a class" do
+        Fabrication::Support.class_for(Object).should == Object
+      end
+
+      it "returns the class for a class name string" do
+        Fabrication::Support.class_for('object').should == Object
+      end
+
+      it "returns the class for a class name symbol" do
+        Fabrication::Support.class_for(:object).should == Object
+      end
+
+    end
+
+    context "with a class that doesn't exist" do
+
+      it "returns nil for a class name string" do
+        Fabrication::Support.class_for('your_mom').should be_nil
+      end
+
+      it "returns nil for a class name symbol" do
+        Fabrication::Support.class_for(:your_mom).should be_nil
+      end
+
+    end
+
+  end
+
+end
