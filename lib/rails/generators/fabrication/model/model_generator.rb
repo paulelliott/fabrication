ca79409f337091b8952784e0a209fea95cf74732
Paul Elliott <paul@hashrocket.com>
2010-09-21 21:24:03 -0400

    Add "_fabicator" to the name of generated files

--- lib/rails/generators/fabrication/model/model_generator.rb
+++ lib/rails/generators/fabrication/model/model_generator.rb
@@ -6,9 +6,9 @@ module Fabrication
       argument :attributes, :type => :array, :default => [], :banner => "field:type field:type"
       class_option :dir, :type => :string, :default => "test/fabricators", :desc => "The directory where the fabricators should go"
       class_option :extension, :type => :string, :default => "rb", :desc => "file extension name"
-        
+
       def create_fabrication_file
-        template 'fabricator.rb', File.join(options[:dir], "#{table_name}.#{options[:extension].to_s}")
+        template 'fabricator.rb', File.join(options[:dir], "#{table_name}_fabricator.#{options[:extension].to_s}")
       end
     end
   end
