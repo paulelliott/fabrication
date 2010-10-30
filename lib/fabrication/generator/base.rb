6583642a708ef81accb221413cc7c00c8ca9a58e
Paul Elliott <paul@hashrocket.com>
2010-10-29 22:54:54 -0400

    Move lazy generation logic to base generator

--- lib/fabrication/generator/base.rb
+++ lib/fabrication/generator/base.rb
@@ -22,8 +22,42 @@ class Fabrication::Generator::Base
     self.klass = klass
   end
 
+  def association?(method_name); false end
+
   def method_missing(method_name, *args, &block)
-    assign(method_name, args.first, &block)
+    if block_given?
+      options = args.first || {}
+      if !options[:force] && association?(method_name)
+        method_name = method_name.to_s
+        count = options[:count] || 0
+
+        # copy the original getter
+        instance.instance_variable_set("@__#{method_name}_original", instance.method(method_name).clone)
+
+        # store the block for lazy generation
+        instance.instance_variable_set("@__#{method_name}_block", block)
+
+        # redefine the getter
+        instance.instance_eval %<
+          def #{method_name}
+            original_value = @__#{method_name}_original.call
+            if @__#{method_name}_block
+              if #{count} \>= 1
+                original_value = #{method_name}= (1..#{count}).map { |i| @__#{method_name}_block.call(self, i) }
+              else
+                original_value = #{method_name}= @__#{method_name}_block.call(self)
+              end
+              @__#{method_name}_block = nil
+            end
+            original_value
+          end
+        >
+      else
+        assign(method_name, options, &block)
+      end
+    else
+      assign(method_name, args.first)
+    end
   end
 
   protected
