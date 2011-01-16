57dde6355e02efd045605dd0f802378699afe689
Paul Elliott <paul@hashrocket.com>
2011-01-15 22:50:02 -0500

    Remove tons of code from generated features

--- lib/rails/generators/fabrication/cucumber_steps/templates/fabrication_steps.rb
+++ lib/rails/generators/fabrication/cucumber_steps/templates/fabrication_steps.rb
@@ -67,27 +67,8 @@ Given /^the following ([^"]*):$/ do |model_name, table|
 end
 
 Given /^that ([^"]*) has the following ([^"]*):$/ do |parent, child, table|
-  child= child.gsub(/\W+/,'_')
-  is_child_plural = child.pluralize == child
   parent = parent.gsub(/\W+/,'_').downcase.sub(/^_/, '')
   parent_instance = instance_variable_get("@#{parent}")
-  parent_class = parent_instance.class
-  if assoc = parent_class.reflect_on_association(child.to_sym) || parent_class.reflect_on_association(child.pluralize.to_sym)
-    parent = (assoc.options[:as] || parent).to_s
-    child = (assoc.options[:class_name] || assoc.options[:source] || child).to_s
-    # source will always be singular, repluralize based on original argument
-    child = child.pluralize if is_child_plural
-  end
-  if child.classify.constantize.method_defined?(parent.pluralize)
-    create_from_table(child, table, parent.pluralize => [parent_instance])
-  elsif child.classify.constantize.method_defined?(parent)
-    create_from_table(child, table, parent => parent_instance)
-  else
-    create_from_table(child, table)
-    if assoc.macro == :has_many
-      parent_instance.send("#{assoc.name}=", @they)
-    else
-      parent_instance.send("#{assoc.name}=", @they.first)
-    end
-  end
+  child = child.gsub(/\W+/,'_').downcase.singularize
+  create_from_table(child, table, parent => parent_instance)
 end
