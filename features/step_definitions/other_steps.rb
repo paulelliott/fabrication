2050c979fb46669df7bc4fb47aae2c808d705ba5
Paul Elliott and Micah Cooper <dev+paulelliott+snakeleg@hashrocket.com>
2011-08-25 20:55:23 -0400

    Add test for meaningful missing fabricator message

---
+++ features/step_definitions/other_steps.rb
@@ -0,0 +1,11 @@
+When /^I try to fabricate "([^"]*)"$/ do |fabricator_name|
+  @fabricator_name = fabricator_name
+end
+
+Then /^it should tell me that it isn't defined$/ do
+  begin
+    Given "1 #{@fabricator_name}"
+  rescue Exception => e
+    e.message.should == "No Fabricator defined for '#{@fabricator_name}'"
+  end
+end
