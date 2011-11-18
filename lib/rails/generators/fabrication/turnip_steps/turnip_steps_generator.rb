3cee389a497d55c46df4e43a6e29e67dce1ec746
Brian McManus <bdmac97@gmail.com>
2011-11-18 01:50:31 -0500

    Allow override of turnip step dir for generator command

--- lib/rails/generators/fabrication/turnip_steps/turnip_steps_generator.rb
+++ lib/rails/generators/fabrication/turnip_steps/turnip_steps_generator.rb
@@ -2,14 +2,20 @@ require 'rails/generators/base'
 
 module Fabrication::Generators
   class TurnipStepsGenerator < Rails::Generators::Base
-
+    argument :step_dir, :type => :string, :default => "spec/acceptance/steps/"
+    
     def generate
-      template 'fabrication_steps.rb', "spec/acceptance/steps/fabrication_steps.rb"
+      template 'fabrication_steps.rb', turnip_step_dir
     end
 
     def self.source_root
       @_fabrication_source_root ||= File.expand_path(File.join(File.dirname(__FILE__), 'templates'))
     end
-
+    
+    private
+    
+    def turnip_step_directory
+      File.join(step_dir, 'fabrication_steps.rb')
+    end
   end
 end
