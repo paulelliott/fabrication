0d07ed51845f4a6cc36ada463062a4c72039d57b
hoverlover <hoverlover@gmail.com>
2012-02-21 10:02:29 -0600

    Fixed module declaration for turnip steps generator.

--- lib/rails/generators/fabrication/turnip_steps/turnip_steps_generator.rb
+++ lib/rails/generators/fabrication/turnip_steps/turnip_steps_generator.rb
@@ -1,21 +1,23 @@
 require 'rails/generators/base'
 
-module Fabrication::Generators
-  class TurnipStepsGenerator < Rails::Generators::Base
-    argument :step_dir, :type => :string, :default => "spec/acceptance/steps/"
+module Fabrication
+  module Generators
+    class TurnipStepsGenerator < Rails::Generators::Base
+      argument :step_dir, :type => :string, :default => "spec/acceptance/steps/"
 
-    def generate
-      template 'fabrication_steps.rb', turnip_step_directory
-    end
+      def generate
+        template 'fabrication_steps.rb', turnip_step_directory
+      end
 
-    def self.source_root
-      @_fabrication_source_root ||= File.expand_path(File.join(File.dirname(__FILE__), 'templates'))
-    end
+      def self.source_root
+        @_fabrication_source_root ||= File.expand_path(File.join(File.dirname(__FILE__), 'templates'))
+      end
 
-    private
+      private
 
-    def turnip_step_directory
-      File.join(step_dir, 'fabrication_steps.rb')
+      def turnip_step_directory
+        File.join(step_dir, 'fabrication_steps.rb')
+      end
     end
   end
 end
