85c3ad0dcc0144834a6b07de74668b422d145930
Matt Conway and Paul Elliott <dev+mattonrails+paulelliott@hashrocket.com>
2012-06-01 16:28:20 -0400

    Handle namespaced class names in generator
    [closes #47]

--- lib/rails/generators/fabrication/model/templates/fabricator.rb
+++ lib/rails/generators/fabrication/model/templates/fabricator.rb
@@ -1,4 +1,4 @@
-Fabricator(:<%= singular_name %>) do
+Fabricator(<%= class_name.match(/::/) ? "'#{class_name}'" : ":#{singular_name}" %>) do
 <% attributes.each do |attribute| -%>
   <%= attribute.name %> <%= attribute.default.inspect %>
 <% end -%>
