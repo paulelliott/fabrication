a21266db485686337c903f29ef03c9ffa85df206
Paul Elliott <paul@codingfrontier.com>
2012-08-23 09:39:52 -0400

    Override set_attributes for sequel models

--- lib/fabrication/generator/sequel.rb
+++ lib/fabrication/generator/sequel.rb
@@ -4,6 +4,10 @@ class Fabrication::Generator::Sequel < Fabrication::Generator::Base
     defined?(Sequel) && klass.ancestors.include?(Sequel::Model)
   end
 
+  def set_attributes
+    __instance.set_fields(__attributes, __attributes.keys)
+  end
+
   def persist
     __instance.save
   end
