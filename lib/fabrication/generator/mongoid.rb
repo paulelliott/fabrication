5b58068a5175e06e7181407fd1ddedda07afdbbd
Elliott Mason <sireleo@gmail.com>
2013-01-07 11:36:44 -0500

    Fix build parameters for mongoid 2.x line

--- lib/fabrication/generator/mongoid.rb
+++ lib/fabrication/generator/mongoid.rb
@@ -5,7 +5,11 @@ class Fabrication::Generator::Mongoid < Fabrication::Generator::Base
   end
 
   def build_instance
-    self.__instance = __klass.new(__attributes, without_protection: true)
+    if Gem::Version.new(Mongoid::VERSION) >= Gem::Version.new('2.3.0')
+      self.__instance = __klass.new(__attributes, without_protection: true)
+    else
+      self.__instance = __klass.new(__attributes)
+    end
   end
 
 end
