1601ef57ba46216ba14754dd841d86ba441133e3
Elliott Mason <sireleo@gmail.com>
2013-01-07 12:09:51 -0500

    actually the version of Mongoid should be greater than or equal to 2.3.0

--- lib/fabrication/generator/mongoid.rb
+++ lib/fabrication/generator/mongoid.rb
@@ -5,7 +5,7 @@ class Fabrication::Generator::Mongoid < Fabrication::Generator::Base
   end
 
   def build_instance
-    if Gem::Version.new(Mongoid::VERSION) >= Gem::Version.new('2.2.0')
+    if Gem::Version.new(Mongoid::VERSION) >= Gem::Version.new('2.3.0')
       self.__instance = __klass.new(__attributes, without_protection: true)
     else
       self.__instance = __klass.new(__attributes)
