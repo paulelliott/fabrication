c772f8779a0372e19f21ff73ed99269dd2588c3f
Kyle Drake <kyledrake@gmail.com>
2013-03-25 11:19:45 -0700

    More granular check for ActiveSupport::HashWithIndifferentAccess

--- lib/fabrication/config.rb
+++ lib/fabrication/config.rb
@@ -30,8 +30,8 @@ module Fabrication
       @path_prefix ||= defined?(Rails) ? Rails.root : "."
     end
 
-    def active_support?
-      @active_support ||= defined?(ActiveSupport)
+    def active_support_hash?
+      @active_support ||= defined?(ActiveSupport::HashWithIndifferentAccess)
     end
 
     attr_writer :register_with_steps
