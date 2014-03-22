5d8ca3537f5a304d6da1743b883397630b5c8b20
Yuki Nishijima <mail@yukinishijima.net>
2014-03-22 15:28:34 -0700

    UnfabricatableError should include the message of the original exception
    When custom #const_missing is defined and it is possible to get
    NameError/NoMethodError from inside #const_missing. This is very
    important since widely-used ActiveSupport::Dependencies's
    "autoload-like" feature is made possible using #const_missing and
    there's a lot of things that will whappen when it's called.
    
    Elevate the original message so we can easily understand what's going on
    in #const_missing.
    
    (For more about Module#const_missing, see:
    http://ruby-doc.org/core-2.1.1/Module.html#method-i-const_missing)

--- spec/fabrication/support_spec.rb
+++ spec/fabrication/support_spec.rb
@@ -32,8 +32,21 @@ describe Fabrication::Support do
           should raise_error(Fabrication::UnfabricatableError)
       end
 
-    end
+      context "and custom const_missing is defined" do
+        before do
+          module Family
+            def self.const_missing(name)
+              raise NameError, "original message"
+            end
+          end
+        end
 
+        it "raises an exception with the message from the original exception" do
+          lambda { Fabrication::Support.class_for("Family::Mom") }.
+            should raise_error(Fabrication::UnfabricatableError, /original message/)
+        end
+      end
+    end
   end
 
   describe ".find_definitions" do
