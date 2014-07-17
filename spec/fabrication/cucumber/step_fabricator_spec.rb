0b94dbd94214d54846e112670dccf9397f78027e
Daniel Spector <daniel.spector@lifebooker.com>
2014-07-17 12:54:54 -0400

    Convert specs to RSpec 2.14.8 syntax with Transpec
    This conversion is done by Transpec 2.3.4 with the following command:
        transpec --keep its --keep oneliner --keep have_items
    
    * 122 conversions
        from: obj.should
          to: expect(obj).to
    
    * 93 conversions
        from: == expected
          to: eq(expected)
    
    * 10 conversions
        from: lambda { }.should
          to: expect { }.to
    
    * 10 conversions
        from: obj.should_receive(:message)
          to: expect(obj).to receive(:message)
    
    * 6 conversions
        from: obj.should_not
          to: expect(obj).not_to
    
    * 6 conversions
        from: obj.stub(:message)
          to: allow(obj).to receive(:message)
    
    * 5 conversions
        from: === expected
          to: be === expected
    
    * 1 conversion
        from: obj.should_not_receive(:message)
          to: expect(obj).not_to receive(:message)
    
    For more details: https://github.com/yujinakayama/transpec#supported-conversions

--- spec/fabrication/cucumber/step_fabricator_spec.rb
+++ spec/fabrication/cucumber/step_fabricator_spec.rb
@@ -9,7 +9,7 @@ describe Fabrication::Cucumber::StepFabricator do
       let(:fabricator_name) { :dog }
 
       before do
-        Fabricate.stub(:schematic).with(fabricator_name).and_return(double(klass: "Boom"))
+        allow(Fabricate).to receive(:schematic).with(fabricator_name).and_return(double(klass: "Boom"))
       end
 
       it { should == "Boom" }
@@ -33,12 +33,12 @@ describe Fabrication::Cucumber::StepFabricator do
     let(:fabricator) { Fabrication::Cucumber::StepFabricator.new(name) }
 
     it "fabricates n times" do
-      Fabricate.should_receive(:create).with(:dog, {}).exactly(n).times
+      expect(Fabricate).to receive(:create).with(:dog, {}).exactly(n).times
       fabricator.n n
     end
 
     it "fabricates with attrs" do
-      Fabricate.should_receive(:create).
+      expect(Fabricate).to receive(:create).
         with(:dog, :collar => 'red').at_least(1)
       fabricator.n n, :collar => 'red'
     end
@@ -46,18 +46,18 @@ describe Fabrication::Cucumber::StepFabricator do
     context 'with a plural subject' do
       let(:name) { 'dogs' }
       it 'remembers' do
-        Fabricate.stub(:create).and_return("dog1", "dog2", "dog3")
+        allow(Fabricate).to receive(:create).and_return("dog1", "dog2", "dog3")
         fabricator.n n
-        Fabrication::Cucumber::Fabrications[name].should == ["dog1", "dog2", "dog3"]
+        expect(Fabrication::Cucumber::Fabrications[name]).to eq(["dog1", "dog2", "dog3"])
       end
     end
 
     context 'with a singular subject' do
       let(:name) { 'dog' }
       it 'remembers' do
-        Fabricate.stub(:create).and_return("dog1")
+        allow(Fabricate).to receive(:create).and_return("dog1")
         fabricator.n 1
-        Fabrication::Cucumber::Fabrications[name].should == 'dog1'
+        expect(Fabrication::Cucumber::Fabrications[name]).to eq('dog1')
       end
     end
 
@@ -66,16 +66,16 @@ describe Fabrication::Cucumber::StepFabricator do
   describe '#from_table' do
     it 'maps column names to attribute names' do
       table = double(hashes: [{ 'Favorite Color' => 'pink' }])
-      Fabricate.should_receive(:create).with(:bear, :favorite_color => 'pink')
+      expect(Fabricate).to receive(:create).with(:bear, :favorite_color => 'pink')
       Fabrication::Cucumber::StepFabricator.new('bears').from_table(table)
     end
 
     context 'with table transforms' do
       let(:table) { double(hashes: [{ 'some' => 'thing' }]) }
-      before { Fabricate.stub(:create) }
+      before { allow(Fabricate).to receive(:create) }
 
       it 'applies transforms' do
-        Fabrication::Transform.should_receive(:apply_to).
+        expect(Fabrication::Transform).to receive(:apply_to).
           with('bears', {:some => 'thing'}).and_return({})
         Fabrication::Cucumber::StepFabricator.new('bears').from_table(table)
       end
@@ -88,14 +88,14 @@ describe Fabrication::Cucumber::StepFabricator do
          {'some' => 'panother'}]
       end
       it 'fabricates with each rows attributes' do
-        Fabricate.should_receive(:create).with(:dog, {:some => 'thing'})
-        Fabricate.should_receive(:create).with(:dog, {:some => 'panother'})
+        expect(Fabricate).to receive(:create).with(:dog, {:some => 'thing'})
+        expect(Fabricate).to receive(:create).with(:dog, {:some => 'panother'})
         Fabrication::Cucumber::StepFabricator.new(name).from_table(table)
       end
       it 'remembers' do
-        Fabricate.stub(:create).and_return('dog1', 'dog2')
+        allow(Fabricate).to receive(:create).and_return('dog1', 'dog2')
         Fabrication::Cucumber::StepFabricator.new(name).from_table(table)
-        Fabrication::Cucumber::Fabrications[name].should == ["dog1", "dog2"]
+        expect(Fabrication::Cucumber::Fabrications[name]).to eq(["dog1", "dog2"])
       end
     end
 
@@ -106,13 +106,13 @@ describe Fabrication::Cucumber::StepFabricator do
         {'some' => 'thing'}
       end
       it 'fabricates with each row as an attribute' do
-        Fabricate.should_receive(:create).with(:dog, {:some => 'thing'})
+        expect(Fabricate).to receive(:create).with(:dog, {:some => 'thing'})
         Fabrication::Cucumber::StepFabricator.new(name).from_table(table)
       end
       it 'remembers' do
-        Fabricate.stub(:create).and_return('dog1')
+        allow(Fabricate).to receive(:create).and_return('dog1')
         Fabrication::Cucumber::StepFabricator.new(name).from_table(table)
-        Fabrication::Cucumber::Fabrications[name].should == "dog1"
+        expect(Fabrication::Cucumber::Fabrications[name]).to eq("dog1")
       end
     end
   end
