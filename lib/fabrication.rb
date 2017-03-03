b470cd817e1973c14e3253cd76606cf934942ca1
Yuki Nishijima <mail@yukinishijima.net>
2017-03-03 17:17:52 -0500

    Do not include Rake::DSL in the top-level scope
    fixes #289.
    
    The commit fd0424bf changed the `lib/fabrication.rb` file to include
    the `Rake::DSL` module, but it turns out that this call is unnecessary
    since Fabrication's rake definition is in fact compatible with both of
    Rake 0.9.* and 10.*. Also, it had brought the deprecated feature back
    in without showing any deprecation warnings.
    
    This commit removes the include call since it is 100% safe to do so.

--- lib/fabrication.rb
+++ lib/fabrication.rb
@@ -4,7 +4,6 @@ autoload :Fabricate, 'fabricate'
 
 if defined?(Rake)
   require 'rake'
-  include Rake::DSL
   Dir[File.join(File.dirname(__FILE__), 'tasks', '**/*.rake')].each { |rake| load rake }
 end
 
