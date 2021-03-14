bf48f275801e910b2b4f968ce3c6ff1edf2f1294
Paul Elliott <paul@codingfrontier.com>
2021-03-14 11:39:14 -0400

    Enable Style/RedundantBegin

--- features/step_definitions/other_steps.rb
+++ features/step_definitions/other_steps.rb
@@ -3,9 +3,7 @@ When(/^I try to fabricate "([^"]*)"$/) do |fabricator_name|
 end
 
 Then(/^it should tell me that it isn't defined$/) do
-  begin
-    step "1 #{@fabricator_name}"
-  rescue StandardError => e
-    e.message.should == "No Fabricator defined for '#{@fabricator_name}'"
-  end
+  step "1 #{@fabricator_name}"
+rescue StandardError => e
+  e.message.should == "No Fabricator defined for '#{@fabricator_name}'"
 end
