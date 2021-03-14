9b98523b60aab51bc008f1a68a96cfed249a5348
Paul Elliott <paul@codingfrontier.com>
2021-03-14 15:54:23 -0400

    Enable RSpec/ExampleWording

--- spec/fabrication/syntax/make_spec.rb
+++ spec/fabrication/syntax/make_spec.rb
@@ -3,20 +3,20 @@ require 'fabrication/syntax/make'
 
 describe Fabrication::Syntax::Make do
   describe '#make mongoid', depends_on: :mongoid do
-    it 'should return a fabricated object' do
+    it 'returns a fabricated object' do
       expect(ParentMongoidDocument.make).to be_instance_of ParentMongoidDocument
     end
 
-    it 'should overwrite options' do
+    it 'overwrites options' do
       expect(ParentMongoidDocument.make(string_field: 'N.Rodrigues').string_field).to eql('N.Rodrigues')
     end
 
-    it 'should treat a first non-hash argument as fabrication name suffix' do
+    it 'treats a first non-hash argument as fabrication name suffix' do
       Fabricator(:parent_mongoid_document_with_handle, from: :parent_mongoid_document)
       expect(ParentMongoidDocument.make(:with_handle).string_field).to eql('content')
     end
 
-    it 'should work the same as Fabricate.build' do
+    it 'works the same as Fabricate.build' do
       expect(ParentMongoidDocument.make).to be_new_record
     end
 
@@ -26,11 +26,11 @@ describe Fabrication::Syntax::Make do
   end
 
   describe '#make activerecord', depends_on: :active_record do
-    it 'should return a fabricated object' do
+    it 'returns a fabricated object' do
       expect(ParentActiveRecordModel.make).to be_instance_of ParentActiveRecordModel
     end
 
-    it 'should work the same as Fabricate.build' do
+    it 'works the same as Fabricate.build' do
       expect(ParentActiveRecordModel.make).to be_new_record
     end
 
@@ -40,11 +40,11 @@ describe Fabrication::Syntax::Make do
   end
 
   describe '#make sequel', depends_on: :sequel do
-    it 'should return a fabricated object' do
+    it 'returns a fabricated object' do
       expect(ParentSequelModel.make).to be_instance_of ParentSequelModel
     end
 
-    it 'should be the same as Fabricate.build' do
+    it 'is the same as Fabricate.build' do
       expect(ParentSequelModel.make).to eql(Fabricate.build(:parent_sequel_model))
     end
 
