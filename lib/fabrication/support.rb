dc97aedca1546873f20046607967c20468d619e0
Paul Elliott <paul@codingfrontier.com>
2021-03-14 11:37:28 -0400

    Enable Style/RegexpLiteral

--- lib/fabrication/support.rb
+++ lib/fabrication/support.rb
@@ -39,7 +39,7 @@ class Fabrication::Support
     end
 
     def variable_name_to_class_name(name)
-      name.to_s.gsub(/\/(.?)/) { "::#{$1.upcase}" }.gsub(/(?:^|_)(.)/) { $1.upcase }
+      name.to_s.gsub(%r{/(.?)}) { "::#{$1.upcase}" }.gsub(/(?:^|_)(.)/) { $1.upcase }
     end
 
     def find_definitions
